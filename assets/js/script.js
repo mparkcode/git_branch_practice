ffcab7701c44d081acdb630fda9d8b5e1795c6e8
Michael Park <michael@codeinstitute.net>
2026-06-22 14:57:59 +0900

    simplified js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,14 @@ const scoreDisplay = document.getElementById("score");
 const increaseButton = document.getElementById("increase");
 const decreaseButton = document.getElementById("decrease");
 
-function updateScore() {
-    scoreDisplay.textContent = score;
-}
 
 increaseButton.addEventListener("click", () => {
     score++;
-    updateScore();
+    scoreDisplay.textContent = score;
 });
 
 decreaseButton.addEventListener("click", () => {
     score--;
-    updateScore();
+    scoreDisplay.textContent = score;
 });
 
-updateScore();
